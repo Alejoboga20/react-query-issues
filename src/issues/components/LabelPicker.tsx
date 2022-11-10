3c1756bfcbf66e2b0873963f244e8fd9d819dce3
Alejo Boga <alejoboga20@gmail.com>
2022-11-10 10:38:10 -0500

    Select data

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,7 +1,8 @@
+import { FC } from 'react';
 import { useLabels } from '../../hooks/useLabels';
 import { LoadingIcon } from '../../shared/components/LoadingIcon';
 
-export const LabelPicker = () => {
+export const LabelPicker: FC<LabelPickerProps> = ({ selectedLabels, onChange }) => {
 	const labelsQuery = useLabels();
 
 	if (labelsQuery.isLoading) return <LoadingIcon />;
@@ -10,9 +11,12 @@ export const LabelPicker = () => {
 		<div>
 			{labelsQuery.data?.map(({ id, color, name }) => (
 				<span
-					className='badge rounded-pill m-1 label-picker'
-					style={{ border: `1px solid #${color}`, color: `#${color}` }}
 					key={id}
+					className={`badge rounded-pill m-1 label-picker ${
+						selectedLabels.includes(name) ? 'label--active' : ''
+					}`}
+					style={{ border: `1px solid #${color}`, color: `#${color}` }}
+					onClick={() => onChange(name)}
 				>
 					{name}
 				</span>
@@ -20,3 +24,8 @@ export const LabelPicker = () => {
 		</div>
 	);
 };
+
+interface LabelPickerProps {
+	selectedLabels: string[];
+	onChange: (labelName: string) => void;
+}
