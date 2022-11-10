05892df0d916df517e7db056785407ecc955d2d2
Alejo Boga <alejoboga20@gmail.com>
2022-11-10 08:25:35 -0500

    Getting labels

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
+
+const getLabels = async () => {
+	const response = await fetch('https://api.github.com/repos/facebook/react/labels');
+	return await response.json();
+};
 
 export const LabelPicker = () => {
-  return (
-    <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
-    </div>
-  )
-}
+	const labelsQuery = useQuery(['labels'], getLabels);
+
+	return (
+		<div>
+			<span
+				className='badge rounded-pill m-1 label-picker'
+				style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+			>
+				Primary
+			</span>
+		</div>
+	);
+};
