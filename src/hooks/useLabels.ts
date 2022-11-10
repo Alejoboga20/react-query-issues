33994ceeb498727b103a7b98f51ed83ca8532a42
Alejo Boga <alejoboga20@gmail.com>
2022-11-10 10:27:03 -0500

    Use Placeholder

--- src/hooks/useLabels.ts
+++ src/hooks/useLabels.ts
@@ -10,7 +10,27 @@ const getLabels = async (): Promise<Label[]> => {
 };
 
 export const useLabels = () => {
-	const labelsQuery = useQuery(['labels'], getLabels, { staleTime: 1000 * 60 * 60 });
+	const labelsQuery = useQuery(['labels'], getLabels, {
+		staleTime: 1000 * 60 * 60,
+		placeholderData: [
+			{
+				id: 710573595,
+				node_id: 'MDU6TGFiZWw3MTA1NzM1OTU=',
+				url: 'https://api.github.com/repos/facebook/react/labels/Component:%20Developer%20Tools',
+				name: 'Component: Developer Tools',
+				color: 'fbca04',
+				default: false,
+			},
+			{
+				id: 127893911,
+				node_id: 'MDU6TGFiZWwxMjc4OTM5MTE=',
+				url: 'https://api.github.com/repos/facebook/react/labels/Component:%20DOM',
+				name: 'Component: DOM',
+				color: 'fef2c0',
+				default: false,
+			},
+		],
+	});
 
 	return labelsQuery;
 };
