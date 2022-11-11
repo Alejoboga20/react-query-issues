c8dbb718112ecf482ec65185c52152ada3e082e1
Alejo Boga <alejoboga20@gmail.com>
2022-11-11 11:25:50 -0500

    Display issues items

--- src/issues/components/IssueList.tsx
+++ src/issues/components/IssueList.tsx
@@ -1,29 +1,31 @@
 import { IssueItem } from './IssueItem';
+import { Issue } from '../../interface/issue';
 
-export const IssueList = () => {
-    return (
-        <div className="card border-white">
-            <div className="card-header bg-dark">
-                <ul className="nav nav-pills card-header-pills">
-                    <li className="nav-item">
-                        <a className="nav-link active">All</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link">Open</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link">Closed</a>
-                    </li>
-                </ul>
-            </div>
-            <div className="card-body text-dark">
-                {
-                    [1,2,3].map( issue => (
-                        <IssueItem key={issue} />
-                    ))
-                
-                }                
-            </div>
-        </div>
-    )
+export const IssueList = ({ issues = [] }: IssueListProps) => {
+	return (
+		<div className='card border-white'>
+			<div className='card-header bg-dark'>
+				<ul className='nav nav-pills card-header-pills'>
+					<li className='nav-item'>
+						<a className='nav-link active'>All</a>
+					</li>
+					<li className='nav-item'>
+						<a className='nav-link'>Open</a>
+					</li>
+					<li className='nav-item'>
+						<a className='nav-link'>Closed</a>
+					</li>
+				</ul>
+			</div>
+			<div className='card-body text-dark'>
+				{issues.map((issue) => (
+					<IssueItem key={issue.id} issue={issue} />
+				))}
+			</div>
+		</div>
+	);
+};
+
+interface IssueListProps {
+	issues: Issue[] | undefined;
 }
